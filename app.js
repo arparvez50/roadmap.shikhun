4c78077075d321c0d5b765f3934fbf1924d71db1
ar_parvez <77061595+arparvez50@users.noreply.github.com>
2023-07-21 18:48:37 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let navigation = document.querySelector(".navigation");
+let navigationBtn = document.querySelector(".navigation-button");
+
+
+navigationBtn.addEventListener("click",function(){
+      navigation.classList.toggle("d-flex");
+})
